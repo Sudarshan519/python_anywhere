6f7adc3a98f31af717f9a0d6b750d55123cfc750
sudarshan519 <sudarshanshrestha519@gmail.com>
2023-08-03 22:08:40 +0545

    connect to another db

---
+++ sqlconnectpython2.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+try:
+    mydb = mysql.connector.connect(
+    host="SudarshanShrestha.mysql.pythonanywhere-services.com",
+    user="SudarshanShresth",
+    password="Asmir123",
+    database="SudarshanShresth$JOBSEARCH"
+    
+    )
+
+    mycursor = mydb.cursor()
+    print("connected")
+    mycursor.execute("SHOW DATABASES")
+    print("query executed")
+    for x in mycursor:
+        print(x) 
+except Exception as e:
+    print(e)
